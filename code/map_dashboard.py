8e2c83a1214cc73b8551c2b69bcac0cd624d5a6d
Knregis <Regiskezia4@gmail.com>
2024-12-05 02:54:25 -0500

    commit 1

--- code/map_dashboard.py
+++ code/map_dashboard.py
@@ -12,3 +12,10 @@ CUSE = (43.0481, -76.1474)  # center of map
 ZOOM = 14                   # zoom level
 VMIN = 1000                 # min value for color scale
 VMAX = 5000                 # max value for color scale
+
+cuse_map = folium.Map(location=CUSE, Zoom_tart=ZOOM)
+df = pd.read_csv('./cache/top_locations_mappable.csv')
+geodf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.lon, df.lat))
+cues_map = geodf.explore(m= cuse_map, marker_type = 'circle' , column='amount', cmap='YlOrRd', legend=True)
+
+sf.folium_static(cuse_map)
